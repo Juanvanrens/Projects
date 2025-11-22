39ac772fe5413a8b111df5fded869515d9e8b4ef
Juan Swanepoel Van Rensburg <juanvanrensburg12@gmail.com>
2025-11-22 14:06:44 +0200

    Refactor print statements for total and per person

--- TipCalc.py
+++ TipCalc.py
@@ -6,12 +6,12 @@ amount  = float(input("What did the bill come to? "))
 perc = int(input("what % would you like to tip"))
 people = int(input("How many people are splitting the bill? "))
 total = amount * perc/100 + amount
-roundedtotal = round(total, 2)
-print(f"The total will be: R", roundedtotal)
+
+print(f"The total will be: R {round(total,2)}")
 
 totalpp = total/people
 
-print(f"The total per person will be: R", round(totalpp,2))
+print(f"The total per person will be: R {round(totalpp,2)}")
 
 
 
